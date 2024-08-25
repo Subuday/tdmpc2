d76eaf5bbefc55a51e6826c21cefc9fab0fa9061
Subuday <mr.gigant977@gmail.com>
2024-08-25 22:59:06 +0300

    Use mps instead of cuda

--- tdmpc2/evaluate.py
+++ tdmpc2/evaluate.py
@@ -1,5 +1,4 @@
 import os
-os.environ['MUJOCO_GL'] = 'egl'
 import warnings
 warnings.filterwarnings('ignore')
 
@@ -39,7 +38,7 @@ def evaluate(cfg: dict):
 		$ python evaluate.py task=dog-run checkpoint=/path/to/dog-1.pt save_video=true
 	```
 	"""
-	assert torch.cuda.is_available()
+	# assert torch.cuda.is_available()
 	assert cfg.eval_episodes > 0, 'Must evaluate at least 1 episode.'
 	cfg = parse_cfg(cfg)
 	set_seed(cfg.seed)
--- tdmpc2/tdmpc2.py
+++ tdmpc2/tdmpc2.py
@@ -16,7 +16,7 @@ class TDMPC2:
 
 	def __init__(self, cfg):
 		self.cfg = cfg
-		self.device = torch.device('cuda')
+		self.device = torch.device('mps')
 		self.model = WorldModel(cfg).to(self.device)
 		self.optim = torch.optim.Adam([
 			{'params': self.model._encoder.parameters(), 'lr': self.cfg.lr*self.cfg.enc_lr_scale},
@@ -30,7 +30,7 @@ class TDMPC2:
 		self.scale = RunningScale(cfg)
 		self.cfg.iterations += 2*int(cfg.action_dim >= 20) # Heuristic for large action spaces
 		self.discount = torch.tensor(
-			[self._get_discount(ep_len) for ep_len in cfg.episode_lengths], device='cuda'
+			[self._get_discount(ep_len) for ep_len in cfg.episode_lengths], device='mps'
 		) if self.cfg.multitask else self._get_discount(cfg.episode_length)
 
 	def _get_discount(self, episode_length):
